782e05683a6c40921e8b97fc7d901f768776784d
ernie <ernie_hs@hotmail.com>
2017-08-05 13:48:20 +0100

    initial revision

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+class Polygon2 {
+  constructor() {
+  }
+}
+
+module.exports.Polygon2 = Polygon2;
+  
---
+++ test.js
@@ -0,0 +1,15 @@
+var Vec2 = require("lib-vec2").Vec2;
+var MatA = require("lib-vec2").MatA;
+var Polygon2 = require("./index").Polygon2;
+var expect = require("chai").expect;
+
+const EPSLON = 0.00001;
+
+describe("Polygon2 is a collection of Vec2's", function() {
+
+  describe("Polygon2 can be constructed", function() {
+
+
+  });
+
+});
